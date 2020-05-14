ba64fdb9200b9db4a4f9e1c7eb70f2af51d0227d
higorasilverio <higorasilverio@outlook.com>
2020-05-14 17:39:36 -0300

    README file

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,13 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("*** Factorial of a number ***");
-		System.out.println();
-		
+				
 		System.out.print("Enter the number to calcule the factorial: ");
 		int control = scanner.nextInt();
-		int factorial = 1;
+		long factorial = 1;
 		for (int i = 1; i <= control; i++) {
 			factorial = factorial * i;
 		}
